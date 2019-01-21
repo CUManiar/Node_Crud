1dd14aa418fbaff7584bd38772c6566b140181fa
chiragma <maniarchirag57@gmail.com>
2019-01-22 00:54:37 +0530

    Updated CRUD operation

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 //Initialize mysql connection
 const mysql = require('mysql');
 
+//Initialize express server
+const express = require('express');
+var app = express();
+const bodyparser = require('body-parser');
+
+//Configure express server
+app.use(bodyparser.json());
+
 //Establish connection to mysql 
+/* NOTE:
+If any Authentication related error comes then run following commands to your mysql
+1. ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '<password>';
+2. FLUSH PRIVILEGES
+*/
 var mysqlConnection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '1234',
     database: 'EmployeeDB',
-    multipleStatements: true
+    multipleStatements: true  //To allow multiple statement
 });
 
 
@@ -16,4 +29,69 @@ mysqlConnection.connect((err) => {
         console.log('DB connection succeded.');
     else
         console.log('DB connection failed \n Error : ' + JSON.stringify(err, undefined, 2));
+});
+
+//Start the express server
+app.listen(3000, () => console.log('Express server is running at port no. : 3000'));
+
+//GET operation
+app.get('/employees', (req, res) => {
+    mysqlConnection.query('select * from employee', (err, rows, fields) => {
+        if (!err)
+            res.send(rows);
+        else
+            res.send(err);
+    })
+});
+
+
+//GET perticular data operation
+app.get('/employees/:id', (req, res) => {
+    mysqlConnection.query('select * from employee where empId = ?',[req.params.id], (err, rows, fields) => {
+        if (!err)
+            res.send(rows);
+        else
+            res.send(err);
+    })
+});
+
+//DELETE perticular data operation
+app.delete('/employees/:id', (req, res) => {
+    mysqlConnection.query('delete from employee where empId = ?',[req.params.id], (err, rows, fields) => {
+        if (!err)
+            res.send('Employee deleted successfully.');
+        else
+            res.send(err);
+    })
+});
+
+//Insert an employee operation
+app.post('/employees', (req, res) => {
+    let emp = req.body;
+    var query = "set @empId = ?;set @name = ?;set @empCode = ?;set @salary = ?; \
+    call employeeInsertOrUpdate(@empID,@name,@empCode,@salary);";
+    var sql = "insert into employee(empId,name,empCode,salary) values (?,?,?,?)";
+    mysqlConnection.query(sql, [emp.empId, emp.name, emp.empCode, emp.salary], (err, result) => {
+        if (!err)
+           res.send('Employee inserted successfully : '+ result);
+        else
+            res.send(err);
+    })
+});
+
+//Update an employee data operation
+app.put('/employees', (req, res) => {
+    let emp = req.body;
+    var query = "set @empId = ?;set @name = ?;set @empCode = ?;set @salary = ?; \
+    call employeeInsertOrUpdate(@empID,@name,@empCode,@salary);";
+    var sql = "update employee set name=?,empCode=?,salary=? where empId = ?";
+    mysqlConnection.query(sql, [emp.name, emp.empCode, emp.salary, emp.empId], (err, rows, fields) => {
+        if (!err)
+            rows.forEach(element => {
+                if(element.constructor == Array)
+                res.send('Employee updated successfully!');
+            });
+        else
+            res.send(err);
+    })
 });
